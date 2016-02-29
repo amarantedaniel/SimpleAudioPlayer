9780eb90e0a54e1ad6498c0e6bbe3e7f75305f1d
Daniel <daniel.amarante2@gmail.com>
2016-02-29 20:23:08 -0300

    [#00] Add first try of framework.

--- SimpleAudioPlayer/SimpleAudioPlayer/ViewController.swift
+++ SimpleAudioPlayer/SimpleAudioPlayer/ViewController.swift
@@ -1,16 +1,32 @@
 import UIKit
 import Jukebox
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, JukeboxDelegate {
+    
+    var jukebox: Jukebox!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let path = NSBundle.mainBundle().pathForResource("Test", ofType: "mp3")
+        let url = NSURL(fileURLWithPath: path!)
+        jukebox = Jukebox(delegate: self, items: [JukeboxItem(URL: url)])
+        jukebox.play()
 
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func jukeboxStateDidChange(jukebox: Jukebox) {
+        
+    }
+    func jukeboxDidLoadItem(jukebox: Jukebox, item: JukeboxItem) {
+        
+    }
+    func jukeboxPlaybackProgressDidChange(jukebox: Jukebox) {
+        
     }
 
 
